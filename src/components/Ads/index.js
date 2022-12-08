6835cd9c28a1004c4aadb064ecc9c656030d576d
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-12-09 00:10:57 +0500

    Completed the multi data rendering. Coudn't do the time ago functionality. Will probably complete tomorrow

---
+++ src/components/Ads/index.js
@@ -0,0 +1,59 @@
+import "./Ads.css";
+import { AiOutlineLike } from "react-icons/ai";
+import { RiShareForwardLine } from "react-icons/ri";
+import { FaRegCommentAlt } from "react-icons/fa";
+
+function Ads({ data }) {
+  // const { fbPostData } = props;
+  // console.log(data);
+
+  return (
+    <>
+      {data.map((item, value) => {
+        return (
+          <div key={value} className='fb-post'>
+            <div className='header'>
+              <img src={item.profilePic} alt='profile' />
+              <div className='profile'>
+                <span className='username'>
+                  <b>{item.username}</b>
+                </span>
+                <span className='date'>{item.date}</span>
+              </div>
+            </div>
+            <div>
+              <p className='post-body'>{item.text}</p>
+            </div>
+            <div className='response'>
+              <div className='likes'></div>
+              <div className='comments'></div>
+            </div>
+            <hr />
+            <div className='feedback'>
+              <button className='like'>
+                <AiOutlineLike
+                  style={{ marginRight: "5px", fontSize: "20px" }}
+                />
+                Like
+              </button>
+              <button className='comment'>
+                <FaRegCommentAlt
+                  style={{ marginRight: "5px", fontSize: "20px" }}
+                />
+                Comment
+              </button>
+              <button className='share'>
+                <RiShareForwardLine
+                  style={{ marginRight: "5px", fontSize: "20px" }}
+                />
+                Share
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default Ads;
---
+++ src/data/FbpostData.js
@@ -0,0 +1,26 @@
+const fbPostData = [
+  {
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+    username: "XYZ",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quam doloribus fugit cupiditate repellendus earum amet quisquam aliquam nostrum veritatis? At, voluptas. Minima cum officia laborum, labore laboriosam nam architecto.",
+    images: [],
+    date: 20221027,
+  },
+  {
+    profilePic: "",
+    username: "",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis quodoloremque animi dolore dolorum voluptate perferendis placeat distinctiopossimus. Beatae, quia ipsam officia suscipit repudiandae corporis sed quod sint ipsum.",
+    images: [],
+    // date: new Date(),
+  },
+  {
+    profilePic: "",
+    username: "",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus idlaborum fugit modi, odio autem in, similique nisi dicta itaque velitveritatis sequi officiis incidunt iure ipsum doloremque quae unde?",
+    images: [],
+    // date: new Date(),
+  },
+];
+
+export default fbPostData;
